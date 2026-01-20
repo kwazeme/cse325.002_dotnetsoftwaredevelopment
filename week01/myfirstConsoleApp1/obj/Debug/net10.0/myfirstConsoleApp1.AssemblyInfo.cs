30ee19fa65224b6b71c92bac783b8dd5d3e37a1a
kwazeme <97014623+kwazeme@users.noreply.github.com>
2026-01-21 01:33:16 +0200

    Week03 - Blazor Web app: Done

--- week01/myfirstConsoleApp1/obj/Debug/net10.0/myfirstConsoleApp1.AssemblyInfo.cs
+++ week01/myfirstConsoleApp1/obj/Debug/net10.0/myfirstConsoleApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("myfirstConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ad76483c2690f181df78dec2cd5fc14b001b979b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eed23e1caff2ad3c65d977a51ca6b287847c55a8")]
 [assembly: System.Reflection.AssemblyProductAttribute("myfirstConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("myfirstConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
