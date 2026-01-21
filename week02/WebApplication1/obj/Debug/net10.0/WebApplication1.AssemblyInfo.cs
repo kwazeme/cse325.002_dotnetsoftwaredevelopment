aefdf855feec0d3e64549775e0d9da4bbe4e85c6
kwazeme <97014623+kwazeme@users.noreply.github.com>
2026-01-21 02:12:05 +0200

    week03 - Blazor Web App - Done

--- week02/WebApplication1/obj/Debug/net10.0/WebApplication1.AssemblyInfo.cs
+++ week02/WebApplication1/obj/Debug/net10.0/WebApplication1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eed23e1caff2ad3c65d977a51ca6b287847c55a8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fbf6f923cdb6a9fe40885319f093fc1969e3d516")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
