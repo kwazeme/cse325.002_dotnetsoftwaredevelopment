83dd250ed4fbe129def453024e63382d428e020f
kwazeme <97014623+kwazeme@users.noreply.github.com>
2026-01-21 02:43:42 +0200

    week03 - Blazor Pizza App

---
+++ week03/BlazingPizza2/SeedData.cs
@@ -0,0 +1,68 @@
+namespace BlazingPizza;
+
+public static class SeedData
+{
+    public static void Initialize(PizzaStoreContext db)
+    {
+        var specials = new PizzaSpecial[]
+        {
+            new PizzaSpecial()
+            {
+                Name = "Basic Cheese Pizza",
+                Description = "It's cheesy and delicious. Why wouldn't you want one?",
+                BasePrice = 90.99m,
+                ImageUrl = "img/pizzas/cheese.jpg",
+            },
+            new PizzaSpecial()
+            {
+                Id = 2,
+                Name = "The Baconatorizor",
+                Description = "It has EVERY kind of bacon",
+                BasePrice = 110.99m,
+                ImageUrl = "img/pizzas/bacon.jpg",
+            },
+            new PizzaSpecial()
+            {
+                Id = 3,
+                Name = "Classic pepperoni",
+                Description = "It's the pizza you grew up with, but Blazing hot!",
+                BasePrice = 100.50m,
+                ImageUrl = "img/pizzas/pepperoni.jpg",
+            },
+            new PizzaSpecial()
+            {
+                Id = 4,
+                Name = "Buffalo chicken",
+                Description = "Spicy chicken, hot sauce and bleu cheese, guaranteed to warm you up",
+                BasePrice = 120.75m,
+                ImageUrl = "img/pizzas/meaty.jpg",
+            },
+            new PizzaSpecial()
+            {
+                Id = 5,
+                Name = "Mushroom Lovers",
+                Description = "It has mushrooms. Isn't that obvious?",
+                BasePrice = 110.00m,
+                ImageUrl = "img/pizzas/mushroom.jpg",
+            },
+            new PizzaSpecial()
+            {
+                Id = 7,
+                Name = "Veggie Delight",
+                Description = "It's like salad, but on a pizza",
+                BasePrice = 110.50m,
+                ImageUrl = "img/pizzas/salad.jpg",
+            },
+            new PizzaSpecial()
+            {
+                Id = 8,
+                Name = "Margherita",
+                Description = "Traditional Italian pizza with tomatoes and basil",
+                BasePrice = 90.99m,
+                ImageUrl = "img/pizzas/margherita.jpg",
+            },
+        };
+        db.Specials.AddRange(specials);
+        db.SaveChanges();
+    }
+}
