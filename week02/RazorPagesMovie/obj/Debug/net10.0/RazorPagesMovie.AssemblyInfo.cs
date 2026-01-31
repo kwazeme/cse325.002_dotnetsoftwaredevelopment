1fc1c454b246d0c46c3eae5bf5c5609d1857ea3f
kwazeme <97014623+kwazeme@users.noreply.github.com>
2026-01-31 02:14:51 +0200

    week04 - ConnectFour: Done

--- week02/RazorPagesMovie/obj/Debug/net10.0/RazorPagesMovie.AssemblyInfo.cs
+++ week02/RazorPagesMovie/obj/Debug/net10.0/RazorPagesMovie.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RazorPagesMovie")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6ccd1015411ae2700a348bd6d90743b26f923c4c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+80c6d804451a9096f55798fd89d234d56aeac58b")]
 [assembly: System.Reflection.AssemblyProductAttribute("RazorPagesMovie")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RazorPagesMovie")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
